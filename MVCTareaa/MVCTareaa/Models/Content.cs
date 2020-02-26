e984b52f08c26024aa60252dc44ca6aa2e211552
Rubinke <fernandorfr02@gmail.com>
2020-02-26 12:49:03 -0400

    Modulo de noticias con filtro

---
+++ MVCTareaa/MVCTareaa/Models/Content.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace MVCTareaa.Models
+{
+    public class Content
+    {
+        [Required]
+        public string Contenido { get; set; }
+    }
+
+    public enum Contenidos
+    {
+        Noticias,
+        Música,
+        Películas
+    }
+}
